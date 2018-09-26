11209af6c12cf548e92876c9ba16bcaacd752edf
rmoctezumacstk <raul.moctezuma@softtek.com>
2018-09-26 17:00:00 -0500

    (TFS #415) RDL2: Implementación de referencia Pruebas Automatizada CREATE (Angular, Clarity y Cypress) Page Object - Se sube implementación de Create para ejemplo de Afiliado y se sube clase Common para metodos comunes que compartiran las clases que realizan operaciones CRUD

--- cypress/pageobject/addafiliado.js
+++ cypress/pageobject/addafiliado.js
@@ -1,16 +1,42 @@
-import Common from '../../pageobject/common.js'
+import Common from '../pageobject/common.js'
 
-class AddAfiliado extends CommonPage{
+export default class AddAfiliado extends Common{
 	
-	this.add = function(values){
-		// *** This commented code iterates values from a dictionary object ***
-		// Object.keys(values).forEach(function(key){
-			// console.log(values[key])
-		// })
+	add(values){
+		cy.get('button')
+		  .contains(/\bAgregar\b/)
+		  .click()
+
+		cy.url()
+		  .should('contain', '/Afiliado/agregar')
+
+		Object.keys(values).forEach(function(key) {
+			if(key.split('_')[0] == 'f'){
+				cy.get('#' + key.split('_')[1]).type(values[key])
+			} else if (key.split('_')[0] == 's') {
+				cy.get('#' + key.split('_')[1]).within(() => {
+					cy.get('clr-radio-wrapper').contains(values[key]).within(() => {
+						cy.get('input[type="radio"]').check()
+					})
+				})
+			} else if (key.split('_')[0] == 'm') {
+				// Code Here
+			} else if (key.split('_')[0] == 'fl') {
+				cy.uploadFile('#' + key.split('_')[1], values[key])
+			}
+		})
+		
+		cy.get('button').contains(/\bAgregar\b/).click()
 	}
 	
-	this.validateMessage = function(){
-		//Code Here
+	validateMessage(){
+		cy.get('div.swal2-popup.swal2-modal.swal2-show>div.swal2-content')
+		  .should('contain', 'Etiquetaasignada save successfully.')
+		  
+		cy.wait(1000)
+		
+		cy.get('div.swal2-popup.swal2-modal.swal2-show>div.swal2-actions>button.swal2-confirm.swal2-styled')
+		  .click()
 	}
 	
 }
