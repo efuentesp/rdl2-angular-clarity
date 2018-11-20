91040fca516de51e9f0345ec124bd9d6ca20cefc
SOFTTEK\normaysel.carbajal <normaysel.carbajal@softtek.com>
2018-11-19 19:14:45 -0600

    Se cambian las ramas para dejar en master el código base

--- src/dev/src/app/tipopension/inventory/pokemon.ts
+++
@@ -1,4 +0,0 @@
-export interface Pokemon {
-  number: number;
-  name: string;
-}
--- src/dev/src/app/tipopension/tipopension.demo.model.ts
+++
@@ -1,7 +0,0 @@
-import { DecimalPipe } from '@angular/common';
-
-export class Tipopension {
-  id: number = null;
-  clave: string = '';
-  nombre: string = '';
-}
