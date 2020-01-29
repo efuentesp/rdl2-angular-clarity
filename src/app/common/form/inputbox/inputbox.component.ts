ab45f93a77f3d29ffa688b214488c5da7244ddbd
SeleneEG <selene.estevez.gamez@gmail.com>
2020-01-29 09:52:25 -0600

    Se añade al componente inputbox la validación de longitud mínima y máxima de texto. Se modifica el margen superior del simbolo de exclamación, que aparece en los mensajes de error, para que no se sobreponga con el campo siguiente.

--- src/app/common/form/inputbox/inputbox.component.ts
+++ src/app/common/form/inputbox/inputbox.component.ts
@@ -1,38 +1,40 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { ControlValueAccessor, FormGroup, FormControl, FormControlName } from '@angular/forms';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  ControlValueAccessor,
+  FormGroup,
+  FormControl,
+  FormControlName
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-inputbox',
-  templateUrl: './inputbox.component.html',
-  styleUrls: ['./inputbox.component.css']
+  selector: "app-inputbox",
+  templateUrl: "./inputbox.component.html",
+  styleUrls: ["./inputbox.component.css"]
 })
 export class InputboxComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  @Input() id: string = '';
+  @Input() id: string = "";
   @Input() required: boolean = true;
-  @Input() type: string = ''
-  @Input() label: string = ''
-  @Input() help: string = ''
-  @Input() placeholder: string = ''
-  @Input() error: string = ''
+  @Input() type: string = "";
+  @Input() label: string = "";
+  @Input() help: string = "";
+  @Input() placeholder: string = "";
+  @Input() error: string = "";
   @Input() hidden: boolean = false;
   @Input() disabled: boolean = false;
   @Input() linecount: number = 0;
   @Input() _formGroup: FormGroup;
   @Input() _formControlName: FormControlName;
   @Input() flag: boolean = false;
+  @Input() minlength: number = 0;
+  @Input() maxlength: number = 0;
   @Output() showModal = new EventEmitter<boolean>();
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   openModal() {
-
     this.flag = true;
     this.showModal.emit(this.flag);
-
   }
-
 }
