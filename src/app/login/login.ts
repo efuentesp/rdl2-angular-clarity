894392e912f665f7a650d655f7c23f62f08b0fed
Normaysel <alexydiego@gmail.com>
2020-07-16 14:51:46 -0500

    Se eliminan comentarios y se ajusta el login en su presentación

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,178 +1,77 @@
-import { Component, OnInit, NgZone } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { first } from "rxjs/operators";
-// import { AuthenticationService, AlertService } from "../_services";
+import { Component, OnInit, NgZone, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { Validators, FormGroup, FormControl } from "@angular/forms";
+import { ClrForm } from "@clr/angular";
 import Swal from "sweetalert2";
-import { environment } from "../../environments/environment";
 import { AuthService } from "../services/auth.service";
 import { Usuario } from "../models/usuario";
 
-@Component({ selector: "login", templateUrl: "login.html" })
+@Component({
+  selector: "login",
+  templateUrl: "login.html",
+  styleUrls: ["../app.component.scss"],
+})
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
+  @ViewChild(ClrForm, { static: true }) clrForm;
+
   loading = false;
   submitted = false;
   returnUrl: string;
 
-  titulo: string = "Por favor Sign In!";
+  title: string = "PSG - Softtek";
   usuario: Usuario;
   token: string = "";
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private authService: AuthService,
-    private router: Router
-  ) {
+  constructor(private authService: AuthService, private router: Router) {
     this.usuario = new Usuario();
   }
+
+  loginForm = new FormGroup({
+    email: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required),
+  });
 
   ngOnInit() {
     console.log("LoginComponent");
 
-    this.loginForm = this.formBuilder.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required],
-    });
-
     if (this.authService.isAuthenticated()) {
-      // Swal(
-      //   "Login",
-      //   `Hola ${this.authService.usuario.username} ya estás autenticado!`,
-      //   "info"
-      // );
-      console.log("Usuario ya estás autenticado!");
       this.router.navigate(["/admin"]);
     }
   }
 
-  get f() {
-    return this.loginForm.controls;
-  }
-
   login(): void {
     console.log(this.usuario);
-    if (this.f.email == null || this.f.password == null) {
+    if (
+      this.loginForm.controls.email == null ||
+      this.loginForm.controls.password == null
+    ) {
       console.log("Username o password vacías!");
       return;
     } else {
-      this.usuario.email = this.f.email.value;
-      this.usuario.password = this.f.password.value;
+      this.usuario.email = this.loginForm.controls.email.value;
+      this.usuario.password = this.loginForm.controls.password.value;
     }
 
     this.authService.login(this.usuario).subscribe(
       (response) => {
         this.token = response.access_token;
-        // this.authService.guardaProfile(response.access_token);
-        // this.authService.guardarUsuario(response.access_token);
-        // this.authService.guardarToken(response.access_token);
-        // let usuario = this.authService.usuario;
-        // this.router.navigate(["/clientes"]);
-        // swal(
-        //   "Login",
-        //   `Hola ${usuario.username}, has iniciado sesión con éxito!`,
-        //   "success"
-        // );
-        console.log("has iniciado sesión con éxito!");
         this.authService.getProfile(this.token).subscribe(
           (response) => {
-            //this.token = response.access_token;
             this.authService.guardarUsuario(response, this.token);
-            // this.authService.guardarUsuario(response.access_token);
-            // this.authService.guardarToken(this.token);
-            // this.authService.guardarPermisos(response.permissions);
-            //let usuario = this.authService.usuario;
             this.router.navigate(["/admin"]);
-            // swal(
-            //   "Login",
-            //   `Hola ${usuario.username}, has iniciado sesión con éxito!`,
-            //   "success"
-            // );
-            console.log("has iniciado sesión con éxito!");
           },
           (err) => {
             if (err.status == 400) {
-              // swal("Error Login", "Usuario o clave incorrectas!", "error");
-              console.log("Usuario o clave incorrectas!");
+              Swal.fire("Error...", "¡Usuario o claves incorrectas!", "error");
             }
           }
         );
       },
       (err) => {
         if (err.status == 400) {
-          console.log("Usuario o clave incorrectas!");
-          // swal("Error Login", "Usuario o clave incorrectas!", "error");
+          Swal.fire("Error...", "¡Usuario o claves incorrectas!", "error");
         }
       }
     );
   }
-
-  // loginForm: FormGroup;
-  // loading = false;
-  // submitted = false;
-  // returnUrl: string;
-
-  // constructor(
-  //   private formBuilder: FormBuilder,
-  //   private route: ActivatedRoute,
-  //   private router: Router,
-  //   private authenticationService: AuthenticationService,
-  //   private alertService: AlertService
-  // ) {}
-
-  // ngOnInit() {
-  //   console.log("OnInit() LoginComponent");
-  //   this.loginForm = this.formBuilder.group({
-  //     email: ["", Validators.required],
-  //     password: ["", Validators.required]
-  //   });
-
-  //   this.authenticationService.logout();
-  //   this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
-  // }
-
-  // get f() {
-  //   return this.loginForm.controls;
-  // }
-
-  // onSubmit() {
-  //   console.log("onSubmit() LoginComponent");
-  //   this.submitted = true;
-
-  //   if (this.loginForm.invalid) {
-  //     return;
-  //   }
-
-  //   this.loading = true;
-
-  //   this.authenticationService
-  //     .login(this.f.email.value, this.f.password.value)
-  //     .pipe(first())
-  //     .subscribe(
-  //       data => {
-  //         console.log('this.router.navigate(["admin"])', data);
-  //         this.router.navigate(["admin"]);
-  //       },
-  //       error => {
-  //         this.loading = false;
-  //         this.submitted = false;
-  //         console.log(error);
-  //         if (error === "Unknown Error") {
-  //           Swal.fire(
-  //             "Error en la conexión con el Servidor",
-  //             `Verifique que el servidor ${
-  //               environment.apiUrl
-  //             } este respondiendo.`,
-  //             "error"
-  //           );
-  //         } else {
-  //           Swal.fire("Error", error, "error");
-  //         }
-  //         //this.alertService.error(error);
-  //       }
-  //     );
-
-  //   //this.loading = false;
-  // }
 }
